1c493f669c8a3721a38bb1ef25edcbfc76bb95f4
alettieri <alettieri@gmail.com>
2013-10-03 09:44:27 -0700

    started work on map animations

---
+++ app/medellin/map.php
@@ -0,0 +1,17 @@
+<section id="map" class="cf">
+    <div class="row">
+        <div class="col-5">
+           <div id="interactive-map" data-svg="/images/map-overlay.svg" data-image="/images/medellin-interactive-map.jpg" data-image-width="566" data-image-height="428">
+                
+            </div>
+            <div id="interactive-graph">
+                [GRAPH]
+            </div>
+        </div>
+        <div class="col-6 push-1">
+            <div id="interactive-content">
+                content
+            </div>    
+        </div>
+    </div>
+</section>
