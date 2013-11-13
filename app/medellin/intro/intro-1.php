82a177ff108cd36500a1a02a70714fa3f22e0675
alettieri <alettieri@gmail.com>
2013-11-12 18:04:12 -0800

    Corrected acute

--- app/medellin/intro/intro-1.php
+++ app/medellin/intro/intro-1.php
@@ -1,7 +1,7 @@
 <div id="medellin-intro" class="section more-room">
     <article class="content-area">
         <aside class="col-6 push-6 subpixel">
-            The Northeastern Urban Integration Project in Medelli'n, Colombia, was sponsored by the City of Medelli'n, which receives the award for its vision and ongoing support of this project. The prize also acknowledges the design leadership of Architect Alejandro Echeverri and the role of the agency Empresa de Desarrollo Urbano (EDU) in the design, management, and execution of the project.
+            The Northeastern Urban Integration Project in Medelli'n, Colombia, was sponsored by the City of Medell&iacute;n, which receives the award for its vision and ongoing support of this project. The prize also acknowledges the design leadership of Architect Alejandro Echeverri and the role of the agency Empresa de Desarrollo Urbano (EDU) in the design, management, and execution of the project.
         </aside>
     </article>
 </div>
